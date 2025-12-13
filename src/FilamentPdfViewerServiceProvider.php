966a7b3003ff235cce4b4dabd710321099267fda
Swelem <saif.swelem@cartona.com>
2025-12-13 19:22:03 +0200

    fix: Correctly publish PDF.js web assets in packageBooted method

--- src/FilamentPdfViewerServiceProvider.php
+++ src/FilamentPdfViewerServiceProvider.php
@@ -39,6 +39,9 @@ class FilamentPdfViewerServiceProvider extends PackageServiceProvider
     public function packageBooted(): void
     {
     if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../resources/dist/pdfjs/web' => public_path('vendor/filament-pdf-viewer/pdfjs'),
+            ], 'filament-pdf-viewer-js');
             // Publish PDF.js files
             $this->publishes([
                 __DIR__ . '/../resources/dist/pdfjs' => public_path('vendor/filament-pdf-viewer/pdfjs'),
